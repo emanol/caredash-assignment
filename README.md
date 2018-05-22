492249083184da66bc1df87213f55edab263503f
Imanol Avendaño <imanol.avendano@gmail.com>
2018-05-22 13:52:42 -0400

    submission

--- README.md
+++ README.md
@@ -3,11 +3,20 @@
 ## Setup
 Execute the following terminal commands
 
+First, cd to src/doctor_model/__init__.py and modify the user, password, host, and database fields to connect to a mysql database of your choice.
+
+Then, cd back to the root directory and execute the following command.
+
 ```bash
-. bin/activate
-export FLASK_APP=src/main.py
-flask run
+python3 src/main.py
 ```
+
+## Design
+Scalability: this app uses flask's builtin testing server, which is not designed for a production workload. In order to scale up this app, one should deploy to a proper web server such as Apache or Nginx.
+
+Error Handling: The server will respond with an error message in the event that a doctor/review doesn't exist, or in the event that the JSON field attached to a POST request is not properly formatted. When a post request successfully completes, a copy of the post request as well as a newly generated id field are returned.
+
+Data Model: I replicated the data model as described in the assignment. The name field is not unique for doctors since there are doctors that share the same name.
 
 ## Libraries used
 - mysqlclient
--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from flask import Flask, jsonify, request
-from sqlalchemy.orm import joinedload
 from doctor_model import db, app, Doctor, Review
 
 db.create_all()
 
 @app.route('/')
-def hello_world():
-    print('this function for hello world executed')
-    response = { 'text' : 'Hello, World!' }
+def root():
+    response = { 'text' : 'Caredash!' }
     return jsonify(response)
 
 @app.route('/doctors/', methods=['GET', 'POST'])
@@ -22,25 +20,17 @@ def get_all_doctors():
     return str(query)
 
 def post_doctor(request):
-    # input validation
-    data = {}
+    data = request.get_json()
     try:
-        data = request.form['doctor']
+        data = data['doctor']
+        doctor = Doctor(name=data['name'])
+        db.session.add(doctor)
+        db.session.commit()
+        response = {'id': data['id'], 'name': doctor.id}
+        return jsonify(response)
     except KeyError:
         error = {"error": "invalid post, must have doctor field"}
         return jsonify(error)
-
-    # adds doctor to database
-    doctor = Doctor(name=data['name'])
-    db.session.add(doctor)
-    db.session.commit()
-
-    response = {
-        'id': data['id'],
-        'name': doctor.id
-    }
-
-    return jsonify(response)
 
 @app.route('/doctors/<int:id>', methods=['GET', 'DElETE'])
 def get_or_delete_doctor(id):
@@ -50,7 +40,6 @@ def get_or_delete_doctor(id):
         return delete_one_doctor(id)
 
 def get_one_doctor(id):
-    # input validation
     doctor = Doctor.query.get(id)
     if doctor:
         response = {
@@ -87,20 +76,17 @@ def get_all_reviews(id):
     if doctor:
         response['id'] = doctor.id,
         response['name'] = doctor.name,
-        response['reviews'] = {}
+        response['reviews'] = doctor.reviews
     else:
         error = {'invalid_doctor_id': 'doctor could not be found'}
         return jsonify(error)
-
-    reviews = Doctor.query.joinedload('reviews')
-    for review in reviews:
-        response['reviews'][review.id] = review.desription
 
     return jsonify(response)
 
 def post_review(request, doctor_id):
     try:
-        comment = request.form['review']
+        comment = request.get_json()
+        comment = comment['review']
         error = {}
         if 'description' not in comment:
             error['no_description'] = 'No description present'
@@ -148,3 +134,6 @@ def delete_one_review(doctor, review):
     db.session.commit()
     response = { 'success' : 'Review was succesffuly deleted' }
     return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
